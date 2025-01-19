22619875f5c7ba41fd9057e912f9079b22d9c6c9
Rebeca Kipper <rebecakipper@gmail.com>
2025-01-19 14:58:38 +0100

    feat: create trip type

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Trip = {
+    from: string;
+    to: string;
+    departureLocation: string;
+    arrivalLocation: string;
+    startDate: string;
+    endDate: string;
+    departureTime: string;
+    arrivalTime: string;
+    id: string;
+    bookingNr: string;
+};
